a12be0f2e8297c9eb7c01b11669fd115ae071111
vputrevu1 <66911213+vputrevu1@users.noreply.github.com>
2024-08-01 08:19:05 -0400

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,15 +1,3 @@
-# data "aws_secretsmanager_secret" "example" {
-#   name = "MySecret"
-# }
-
-# data "aws_secretsmanager_secret_version" "example" {
-#   secret_id = data.aws_secretsmanager_secret.example.id
-# }
-
-# locals {
-#   secrets = jsondecode(data.aws_secretsmanager_secret_version.example.secret_string)
-# }
-
 data "aws_s3_bucket" "appbucket" {
     bucket = "app-my-app-bucket" 
 }
